d32515b578163c5bed8c6930bf8651fb1c6b5a4d
Sergio Venegas <69369902+Angcroft@users.noreply.github.com>
2024-08-12 00:03:26 -0400

    Create Interfacer.java

---
+++ javakey/Interfacer.java
@@ -0,0 +1,40 @@
+public class Interfacer {
+    public static void welcomer() {
+        System.out.println("Select one of the following options: ");
+        System.out.println("1. Generate Password");
+        System.out.println("2. Personalize Password");
+        System.out.println("3. Exit");
+    }
+
+    public static void customizer() {
+        System.out.println("Please select the following customizer option: ");
+        System.out.println("1. Substract characters");
+        System.out.println("2. Add Characters");
+        System.out.println("3. Return to main menu");
+    }
+
+    public static void substracter() {
+        System.out.println("Please select what kind of character you want to substract: ");
+        System.out.println("1. Upper");
+        System.out.println("2. Lower");
+        System.out.println("3. Digit");
+        System.out.println("4. Special");
+        System.out.println("5. Cancel");
+    }
+
+    public static void adder() {
+        System.out.println("Please select what kind of character you want to add: ");
+        System.out.println("1. Upper");
+        System.out.println("2. Lower");
+        System.out.println("3. Digit");
+        System.out.println("4. Special");
+        System.out.println("5. Cancel");
+    }
+
+    public static void passGenerated() {
+        System.out.println("Password generated. What you want to do next?: ");
+        System.out.println("1. Generate another password");
+        System.out.println("2. Personalize password");
+        System.out.println("3. Exit");
+    }
+}
